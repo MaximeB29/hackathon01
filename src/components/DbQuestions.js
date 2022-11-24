a75e5351fcb3f9fe55c77ea650935d1f5e70f7b4
Kevin Caron <kevin.caron.91@gmail.com>
2022-11-24 21:59:04 +0100

    affichage question reponse

---
+++ src/components/DbQuestions.js
@@ -0,0 +1,155 @@
+export const DbQuestions = [
+  {
+    id: 1,
+    question: "Thé ou Café ?",
+    answers: [{ response: "Thé" }, { response: "Café" }],
+  },
+  {
+    id: 2,
+    question: "Sucré ou Salé ?",
+    answers: [{ response: "Sucré" }, { response: "Salé" }],
+  },
+  {
+    id: 3,
+    question: "Voiture ou Transport en commun ?",
+    answers: [{ response: "Voiture" }, { response: "Transport en commun" }],
+  },
+  {
+    id: 4,
+    question: "Maillot de bain ou Naturiste ?",
+    answers: [{ response: "Maillot de bain" }, { response: "Naturiste" }],
+  },
+  {
+    id: 5,
+    question: "Frais ou Farniente ?",
+    answers: [{ response: "Frais" }, { response: "Farniente" }],
+  },
+  {
+    id: 6,
+    question: "Burger ou Pizza ?",
+    answers: [{ response: "Burger" }, { response: "Pizza" }],
+  },
+  {
+    id: 7,
+    question: "Plage ou Montagne ?",
+    answers: [{ response: "Plage" }, { response: "Montagne" }],
+  },
+  {
+    id: 8,
+    question: "Vin ou Bière ?",
+    answers: [{ response: "Vin" }, { response: "Bière" }],
+  },
+  {
+    id: 9,
+    question: "PMU ou Etoilé ?",
+    answers: [{ response: "PMU" }, { response: "Etoilé" }],
+  },
+  {
+    id: 10,
+    question: "Randonnée ou Musée ?",
+    answers: [{ response: "Randonnée" }, { response: "Musée" }],
+  },
+  {
+    id: 11,
+    question: "Sunset ou Sunrise ?",
+    answers: [{ response: "Sunset" }, { response: "Sunrise" }],
+  },
+  {
+    id: 12,
+    question: "Spa ou SPA?",
+    answers: [{ response: "Spa" }, { response: "SPA" }],
+  },
+  {
+    id: 13,
+    question: "Sport extrême ou Hamac ?",
+    answers: [{ response: "Sport extrême" }, { response: "Hamac" }],
+  },
+  {
+    id: 14,
+    question: "Marcher ou Aller au marché ?",
+    answers: [{ response: "Marcher" }, { response: "Aller au marché" }],
+  },
+  {
+    id: 15,
+    question: "Rhum ou Whisky ?",
+    answers: [{ response: "Rhum" }, { response: "Whisky" }],
+  },
+  {
+    id: 16,
+    question: "Lève tôt ou Lève tard ?",
+    answers: [{ response: "Lève tôt" }, { response: "Lève tard" }],
+  },
+  {
+    id: 17,
+    question: "Danser toute la nuit ou Se coucher tôt ?",
+    answers: [
+      { response: "Danser toute la nuit" },
+      { response: "Se coucher tôt" },
+    ],
+  },
+  {
+    id: 18,
+    question: "Paysages Ou Immeubles?",
+    answers: [{ response: "Paysages" }, { response: "Immeubles" }],
+  },
+  {
+    id: 19,
+    question: "Aventure ou Repos ?",
+    answers: [{ response: "Aventure" }, { response: "Repos" }],
+  },
+  {
+    id: 20,
+    question: "Désert ou Forêt tropicale ?",
+    answers: [{ response: "Désert" }, { response: "Forêt tropicale" }],
+  },
+  {
+    id: 21,
+    question: "Flûte de pan ou Guitare ?",
+    answers: [{ response: "Flûte de pan" }, { response: "Guitare" }],
+  },
+  {
+    id: 22,
+    question: "Boeuf ou Poisson ?",
+    answers: [{ response: "Boeuf" }, { response: "Poisson" }],
+  },
+  {
+    id: 23,
+    question: "Nature ou Peinture ?",
+    answers: [{ response: "Nature" }, { response: "Peinture" }],
+  },
+  {
+    id: 24,
+    question: "Fromage ou Chocolat ?",
+    answers: [{ response: "Fromage" }, { response: "Chocolat" }],
+  },
+  {
+    id: 25,
+    question: "Négocier ou Payer plein pot ?",
+    answers: [{ response: "Négocier" }, { response: "Payer plein pot" }],
+  },
+  {
+    id: 26,
+    question: "Calme ou Tempête ?",
+    answers: [{ response: "Calme" }, { response: "Tempête" }],
+  },
+  {
+    id: 27,
+    question: "Jour ou Nuit ?",
+    answers: [{ response: "Jour" }, { response: "Nuit" }],
+  },
+  {
+    id: 28,
+    question: "Expression ou Oppression ?",
+    answers: [{ response: "Expression" }, { response: "Oppression" }],
+  },
+  {
+    id: 29,
+    question: "Potatoes ou Frites ?",
+    answers: [{ response: "Potatoes" }, { response: "Frites" }],
+  },
+  {
+    id: 30,
+    question: "Tropique ou Tropico ?",
+    answers: [{ response: "Tropique" }, { response: "Tropico" }],
+  },
+];
