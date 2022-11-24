25baf88cb9fd9bbe9f36b0d044236ee3afa317d5
e-genie <eugenie.froustey@gmail.com>
2022-11-24 19:17:44 +0100

    added logo to navbar and changed colors of navbar & footer to match logo

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="flex h-16 p-2 justify-center bg-blue-300">
+    <div className="flex h-16 p-2 justify-center bg-[#6caad9]">
       <p>©Find My Trip</p>
     </div>
   );
--- src/components/Layout/Navbar/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar/Navbar.jsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="bg-blue-300 h-8">
-      <ul className="list-none flex">
-        <li className="mx-2">
-          <Link to="/">Find my trip</Link>
-        </li>
+    <nav className="bg-[#6caad9] h-16 flex">
+      <ul className="list-none flex items-center ml-2">
         <li>
+          <Link to="/">
+            <img className="h-14" src="src/assets/logo/logo.png" alt="logo" />
+          </Link>
+        </li>
+        <li className="ml-8">
           <Link to="/quiz">Quiz</Link>
         </li>
       </ul>
