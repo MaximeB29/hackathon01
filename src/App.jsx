be6d912f13e992ed0b1122f2c78dc366a8300d25
e-genie <eugenie.froustey@gmail.com>
2022-11-24 15:53:31 +0100

    added navbar & router, created files and directories

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import Quiz from "./components/Quiz/Quiz";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route />
+          <Route path="/quiz" element={<Quiz />} />
         </Route>
       </Routes>
     </BrowserRouter>
