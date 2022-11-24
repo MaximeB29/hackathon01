f46e8acbc9afce0574f7bbc243763f4bf5d55e8a
e-genie <eugenie.froustey@gmail.com>
2022-11-24 23:08:49 +0100

    redirect to results page after answering 10 questions

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Results from "../Results/Results";
 import { DbQuestions } from "/src/components/DbQuestions";
 
 const TOTAL_NUM_QUESTIONS = 10;
@@ -6,13 +7,19 @@ const TOTAL_NUM_QUESTIONS = 10;
 function Quiz() {
   const [currentQuestion, setCurentQuestion] = useState(0);
   const [optionChosen, setOptionChosen] = useState(false);
-  const [score, setScore] = useState(0);
+  const [currentAnswer, setCurrentAnswer] = useState([]);
+  const [endQuiz, setEndQuiz] = useState(false);
 
   const handleAnswerOptionCLick = (isclicked) => {
     if (isclicked) {
+      currentAnswer.push(isclicked);
+      setCurrentAnswer(currentAnswer);
     }
 
     const nextQuestion = currentQuestion + 1;
+    if (currentAnswer.length === 10) {
+      setEndQuiz(true);
+    }
     if (nextQuestion < TOTAL_NUM_QUESTIONS) {
       setCurentQuestion(nextQuestion);
     }
@@ -21,20 +28,24 @@ function Quiz() {
 
   return (
     <div>
-      <h1>QUIZ</h1>
-      <div>
-        <span>Question {currentQuestion + 1}</span>/{TOTAL_NUM_QUESTIONS}
-      </div>
-      <div>{DbQuestions[currentQuestion].question}</div>
-      <div>
-        {DbQuestions[currentQuestion].answers.map((answer) => (
-          <button onClick={() => handleAnswerOptionCLick(answer)}>
-            {answer.response}
-          </button>
-        ))}
-      </div>
-
-      <div></div>
+      {endQuiz ? (
+        <Results />
+      ) : (
+        <div>
+          <h1>QUIZ</h1>
+          <div>
+            <span>Question {currentQuestion + 1}</span>/{TOTAL_NUM_QUESTIONS}
+          </div>
+          <div>{DbQuestions[currentQuestion].question}</div>
+          <div>
+            {DbQuestions[currentQuestion].answers.map((answer) => (
+              <button onClick={() => handleAnswerOptionCLick(answer)}>
+                {answer.response}
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
