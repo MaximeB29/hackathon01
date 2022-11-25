fb53bd7b748919b435871f93a89ba970eb695ce8
e-genie <eugenie.froustey@gmail.com>
2022-11-25 14:29:56 +0100

    XRevert "ok merge"
    This reverts commit cfbdada0115d42b9a1749441d6f0da2b264ad68f.

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -25,43 +25,41 @@ function Quiz() {
       setCurrentAnswer(currentAnswer);
     }
 
-    const handleAnswerOptionCLick = () => {
-      const nextQuestion = currentQuestion + 1;
-      if (currentAnswer.length === 10) {
-        setEndQuiz(true);
-      }
-      if (nextQuestion < TOTAL_NUM_QUESTIONS) {
-        setCurrentQuestion(nextQuestion);
-      }
-      return setOptionChosen(true);
-    };
+  const handleAnswerOptionCLick = () => {
+    const nextQuestion = currentQuestion + 1;
+    if (currentAnswer.length === 10) {
+      setEndQuiz(true);
+    }
+    if (nextQuestion < TOTAL_NUM_QUESTIONS) {
+      setCurrentQuestion(nextQuestion);
+    }
+    return setOptionChosen(true);
   };
 
   return (
-    <div>
-      {endQuiz ? (
-        <Results currentAnswer={currentAnswer} />
-      ) : (
-        <div className="flex flex-col justify-items-center">
-          <h1 className="flex justify-center">Mes préferences de voyage</h1>
-          <div className="mt-11 ml-60 mb-72 h-60 flex flex-col border  w-1/2 rounded-xl place-content-center p-1 bg-slate-400 shadow-xl">
-            <div className="flex justify-center -mt-12">
-              <span>Question {currentQuestion + 1}</span>/{TOTAL_NUM_QUESTIONS}
-            </div>
-            <div className="flex items-start">{randomQuestion.question}</div>
-            <div className="flex flex-col items-end gap-2 mt-10">
-              {randomQuestion.answers.map((answer) => (
-                <button
-                  className="border ring-slate-400 rounded-lg w-1/2 hover:bg-slate-300 "
-                  onClick={() => handleAnswerOptionCLick(answer.response)}
-                >
-                  {answer.response}
-                </button>
-              ))}
-            </div>
-          </div>
+    {endQuiz ? (
+      <Results currentAnswer={currentAnswer} />
+    ) : (
+    <div className="flex flex-col justify-items-center">
+      <h1 className="flex justify-center">Mes préferences de voyage</h1>
+      <div className="mt-11 ml-60 mb-72 h-60 flex flex-col border  w-1/2 rounded-xl place-content-center p-1 bg-slate-400 shadow-xl">
+        <div className="flex justify-center -mt-12">
+          <span>Question {currentQuestion + 1}</span>/{TOTAL_NUM_QUESTIONS}
         </div>
+        <div className="flex items-start">{randomQuestion.question}</div>
+        <div className="flex flex-col items-end gap-2 mt-10">
+          {randomQuestion.answers.map((answer) => (
+            <button
+              className="border ring-slate-400 rounded-lg w-1/2 hover:bg-slate-300 "
+              onClick={() => handleAnswerOptionCLick(answer.response)}
+            >
+              {answer.response}
+            </button>
+          ))}
+        </div>
+      </div>
       )}
+    <div>
     </div>
   );
 }
